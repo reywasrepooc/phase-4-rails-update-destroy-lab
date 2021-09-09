a9ac58eac6cdc0d7a05cd19281d9ffb253ac1d50
Cooper Sawyer <cooper.sawyer@powerhrg.com>
2021-09-09 14:25:04 -0400

    commit tree 7928bececb3e63b436e7f0aca5639626d2513dfa

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -8,7 +8,7 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant
     render json: plant
   end
 
@@ -18,9 +18,27 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
-  private
+  # UPDATE /plants/:id
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant
+  end
+
+  #DELETE /plants/:id
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
+  end
+
+private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+  def find_plant
+    Plant.find(params[:id])
   end
 end
